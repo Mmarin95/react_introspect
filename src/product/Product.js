75dfb9ce57853e14d68ab048728ac732bff7a32b
Miquel Marin <mmarin_79@hotmail.com>
2021-01-10 17:58:01 +0100

    add product special

--- src/product/Product.js
+++ src/product/Product.js
@@ -2,17 +2,30 @@ import React from 'react'
 
 import './Product.css'
 
-const Product = ({ title, description, image }) => {
-  return (
-    <div className="product">
-      <input className="product__checkbox" type="checkbox" id={title} name={title} value=""></input>
-      <img className="product__image" src={image} alt="the description" />
-      <div className="product__block">
-        <span className="product__title">{title}</span>
-        <p className="product__description">{description}</p>
+const Product = ({ title, description, image, isSpecial }) => {
+
+  if (!isSpecial) {
+    return (
+      <div className="product">
+        <input className="product__checkbox" type="checkbox" id={title} name={title} value=""></input>
+        <img className="product__image" src={image} alt="the description" />
+        <div className="product__block">
+          <span className="product__title">{title}</span>
+          <p className="product__description">{description}</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="product product--special">
+        <input className="product__checkbox product__checkbox--special" type="checkbox" id={title} name={title} value=""></input>
+        <img className="product__image product__image--special" src={image} alt="the description" />
+        <div className="product__block product__block--special">
+          <span className="product__description product__description--special">{description}</span>
+        </div>
+      </div>
+    )
+  }
 }
 
 export { Product }
--- src/products/Products.js
+++ src/products/Products.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { Product } from '../product'
 
 import background from './assets/background.jpg'
+import giftIcon from './assets/gift_icon_white.png'
+
 import './Products.css'
 
 const products = [
@@ -10,11 +12,25 @@ const products = [
     title: 'Itaque facere fugiat eum.',
     description: 'Itaque facere fugiat eum. Tempora eum molestiae voluptas consequatur aut corporis quia. Recusandae aut.',
     image: background,
+    isSpecial: false,
   },
   {
     title: 'Itaque facere fugiat eum.',
     description: 'Itaque facere fugiat eum. Tempora eum molestiae voluptas consequatur aut corporis quia. Recusandae aut.',
     image: background,
+    isSpecial: false,
+  },
+  {
+    title: 'Itaque facere fugiat eum.',
+    description: 'Itaque facere fugiat eum. Tempora eum molestiae voluptas consequatur aut corporis quia. Recusandae aut.',
+    image: background,
+    isSpecial: false,
+  },
+  {
+    title: '',
+    description: 'I would like a personal Packaging',
+    image: giftIcon,
+    isSpecial: true,
   }
 ]
 
@@ -29,6 +45,7 @@ const Products = () => {
             title={product.title}
             description={product.description}
             image={product.image}
+            isSpecial={product.isSpecial}
           />
         ))
       }
